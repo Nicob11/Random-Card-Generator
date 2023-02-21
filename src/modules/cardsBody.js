4e261e13c705763c3434b52f51673964c5435c90
NicoB <nicobasma_@hotmail.com>
2023-02-21 21:36:53 +0000

    developing in a new branch with some news styles

--- src/modules/cardsBody.js
+++ src/modules/cardsBody.js
@@ -1,38 +1,23 @@
 // import { generateRandomCard } from "./generateRandomCard";
 
 export function cardBody(hint, numero) {
-  document.getElementById(
-    "cardDiv"
-  ).innerHTML = `<div class="container-fluid d-flex justify-content-center">
-<div class="row">
-<div class="col">
-<div class="randomCard">
-<div class="card">
-<div class="row">
-<div class="col">
-<div class="classCard">
-<h3 class="hint" id="hint-top">${hint}</h3>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="col">
-<div class="bodyCard">
-<h1 class="hint">${numero}</h1>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="col">
-<div class="divclass">
-<h3 class= "hint" id="hint-botton">${hint}</h3>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+  document.getElementById("container_bodyCard").innerHTML = `
+              <div class="classCard" id="cardDiv">
+                <h3 class="hint" id="hint-top">${hint}</h3>
+                <div class="row">
+                  <div class="col">
+                    <div class="bodyCard">
+                      <h1 class="hint">${numero}</h1>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <div class="divclass">
+                    <h3 class="hint" id="hint-botton">${hint}</h3>
+          </div>
+        </div>
+      </div>
 </div>`;
   if (hint === `&#9829` || hint === `&#9830`) {
     const h3 = document.querySelectorAll(".hint");
