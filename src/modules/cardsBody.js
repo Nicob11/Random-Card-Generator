6819a6089779578a3b517399495de4c117f5d450
NicoB <nicobasma_@hotmail.com>
2023-02-23 23:41:31 +0000

    conditional

--- src/modules/cardsBody.js
+++ src/modules/cardsBody.js
@@ -1,13 +1,27 @@
 // import { generateRandomCard } from "./generateRandomCard";
 
 export function cardBody(hint, numero) {
-  document.getElementById("container_bodyCard").innerHTML = `
+  let hintNumber = "";
+  if (numero < 10) {
+    for (let i = 0; i < numero; i++) {
+      hintNumber += hint;
+    }
+  }
+  numero === 10
+    ? (hintNumber = `&#129504`)
+    : numero === 11
+    ? (hintNumber = `&#129504`)
+    : numero === 12
+    ? (hintNumber = `&#129504`)
+    : numero === 13
+    ? (hintNumber = `&#129504`)
+    : (document.getElementById("container_bodyCard").innerHTML = `
               <div class="classCard" id="cardDiv">
                 <h3 class="hint" id="hint-top">${hint}</h3>
                 <div class="row">
                   <div class="col">
                     <div class="bodyCard">
-                      <h1 class="hint">${numero}</h1>
+                      <h1 class="hint">${hintNumber}</h1>
                   </div>
                 </div>
               </div>
@@ -18,7 +32,7 @@ export function cardBody(hint, numero) {
           </div>
         </div>
       </div>
-</div>`;
+</div>`);
   if (hint === `&#9829` || hint === `&#9830`) {
     const h3 = document.querySelectorAll(".hint");
     h3.forEach(elem => {
